f5abf7e6440e89d1dba6d5bb887a6baec8e9d021
Jefferson S Rodrigues <jefferson2jsr@gmail.com>
2023-03-01 17:19:35 +0000

    Correção de import não utilizado

--- src/main/java/com/letscode/Main.java
+++ src/main/java/com/letscode/Main.java
@@ -6,7 +6,6 @@ import com.letscode.service.Statistic;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
